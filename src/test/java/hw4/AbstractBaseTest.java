79eb0f343217156d37ff2d2daf7947925361df77
EvgeniiKlimenko <Broken-head@yandex.ru>
2021-03-31 14:04:21 +0300

    Commit for Jenkins run #2, cut off browser version

--- src/test/java/hw4/AbstractBaseTest.java
+++ src/test/java/hw4/AbstractBaseTest.java
@@ -22,7 +22,8 @@ public abstract class AbstractBaseTest {
 
     @BeforeClass
     public void setUp(ITestContext context) {
-        WebDriverManager.chromedriver().browserVersion("89.0.4389.82").setup();
+        // .browserVersion("89.0.4389.82")
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
